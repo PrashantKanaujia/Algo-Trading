e494c167e4cc6500ca9664608e2913452e2991f1
Prashant <prashantkanaujia2@gmail.com>
2026-05-09 23:00:42 +0530

    changing backend address

--- trading_dashboard/src/services/api.js
+++ trading_dashboard/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "https://algo-trading-oq5l.onrender.com/"
+  // baseURL: "http://localhost:8000"
 });
 
 export const getPositions = () => api.get("/positions");
